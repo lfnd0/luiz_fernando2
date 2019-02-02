ad5285ce08212ff3815562ab7ce2fc41dbd0dc8e
Luiz Fernando <luiz-fsilva37@hotmail.com>
2019-02-02 19:02:46 -0300

    Adição projeto CardShopYGO

--- CardShopYGO/src/main/java/edu/ygo/resources/CardResource.java
+++ CardShopYGO/src/main/java/edu/ygo/resources/CardResource.java
@@ -1,12 +1,9 @@
 package edu.ygo.resources;
 
 import java.util.List;
-//import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.ygo.domain.Card;
@@ -23,11 +20,4 @@ public class CardResource {
 		List <Card> card = cardRepository.findAll();
 		return card;
 	}
-	
-	/*
-	 * @DeleteMapping("cards/{id}") public void deleteCard(@PathVariable int id) {
-	 * Optional<Card> cardFound = cardRepository.findById(id);
-	 * 
-	 * if(cardFound.isPresent()) { cardRepository.deleteById(id); } }
-	 */
 }
--- CardShopYGO/src/main/java/edu/ygo/resources/MonsterResource.java
+++ CardShopYGO/src/main/java/edu/ygo/resources/MonsterResource.java
@@ -1,14 +1,9 @@
 package edu.ygo.resources;
 
 import java.util.List;
-//import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PutMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.ygo.domain.Monster;
